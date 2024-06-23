ab307d71f50d9c01fdce80cac486d6a2f50b9966
Михайло <153002746+mishajr@users.noreply.github.com>
2024-06-24 02:07:07 +0300

    Add files via upload
    C#HW06

---
+++ C#HW06/Card.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_HW06
+{
+     class Card
+    {
+        private string cardNumber;
+        private string cardHolderName;
+        private string cvc;
+        private DateTime expirationDate;
+
+       
+        public string CardNumber
+        {
+            get { return cardNumber; }
+            set
+            {
+                if (value.Length != 16 || !long.TryParse(value, out _))
+                {
+                    throw new ArgumentException("Номер картки повинен містити 16 цифр!");
+                }
+                cardNumber = value;
+            }
+        }
+
+    
+        public string CardHolderName
+        {
+            get { return cardHolderName; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("ПІБ власника не може бути порожнім!");
+                }
+                cardHolderName = value;
+            }
+        }
+
+       
+        public string CVC
+        {
+            get { return cvc; }
+            set
+            {
+                if (value.Length != 3 || !int.TryParse(value, out _))
+                {
+                    throw new ArgumentException("CVC повинен містити 3 цифри!");
+                }
+                cvc = value;
+            }
+        }
+
+       
+        public DateTime ExpirationDate
+        {
+            get { return expirationDate; }
+            set
+            {
+                if (value < DateTime.Now)
+                {
+                    throw new ArgumentException("Дата завершення дії картки не може бути в минулому!");
+                }
+                expirationDate = value;
+            }
+        }
+
+       
+        public Card(string cardNumber, string cardHolderName, string cvc, DateTime expirationDate)
+        {
+            CardNumber = cardNumber;
+            CardHolderName = cardHolderName;
+            CVC = cvc;
+            ExpirationDate = expirationDate;
+        }
+
+        
+        public void DisplayCardInfo()
+        {
+            Console.WriteLine($"Номер картки: {CardNumber}");
+            Console.WriteLine($"ПІБ власника: {CardHolderName}");
+            Console.WriteLine($"CVC: {CVC}");
+            Console.WriteLine($"Дата завершення дії картки: {ExpirationDate.ToShortDateString()}");
+        }
+
+    }
+}
---
+++ C#HW06/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Text;
+
+
+
+namespace C_HW06
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+
+            #region 
+            //try
+            //{
+
+
+            //    Console.Write("Введіть набір символів від 0 до 9: ");
+
+            //    string userInput = Console.ReadLine();
+
+                
+            //    int number = int.Parse(userInput);
+
+           
+            //    if (number < int.MinValue || number > int.MaxValue)
+            //    {
+            //        Console.WriteLine($"Число {number} виходить за межі допустимого діапазону типу int.");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"Введене число: {number}");
+            //    }
+
+
+            //}
+            //catch (FormatException)
+            //{
+            //    Console.WriteLine("Введено некоректний набір символів. Будь ласка, введіть тільки цифри від 0 до 9.");
+            //}
+            //catch (OverflowException)
+            //{
+            //    Console.WriteLine("Введене число виходить за межі допустимого діапазону типу int.");
+            //}
+
+            #endregion
+
+
+
+            try
+            {
+     
+                Card card = new Card("1234567812345678", "Іван Іванович Іванов", "123", new DateTime(2025, 12, 31));
+
+               
+                card.DisplayCardInfo();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Помилка: {ex.Message}");
+            }
+
+        }
+    }
+}
